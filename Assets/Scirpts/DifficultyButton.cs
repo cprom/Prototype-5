4b0214380f93544ea01c5ce6866fcde2b62a613c
Chen Prom <chenprom@Chens-MacBook-Pro.local>
2021-02-01 14:28:20 -0600

    complete challenge 5

--- Assets/Scirpts/DifficultyButton.cs
+++ Assets/Scirpts/DifficultyButton.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
     private GameManager gameManager;
     public int difficulty;
-    
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
